1d9341e8012961dd61760637abab6281cd032e03
lzy20247 <673170360@qq.com>
2022-08-26 17:17:09 +0800

    fix:修复 操作时间字段

--- src/main/java/com/jtyjy/finance/manager/bean/BudgetExtractCommissionApplicationLog.java
+++ src/main/java/com/jtyjy/finance/manager/bean/BudgetExtractCommissionApplicationLog.java
@@ -46,12 +46,6 @@ public class BudgetExtractCommissionApplicationLog implements Serializable {
     private String remarks;
 
     /**
-     *  操作时间
-     */
-    @TableField(value = "financial_director_node_operate_time")
-    private Date financialDirectorNodeOperateTime;
-
-    /**
      * 操作人
      */
     @TableField(value = "create_by")
@@ -66,59 +60,4 @@ public class BudgetExtractCommissionApplicationLog implements Serializable {
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        BudgetExtractCommissionApplicationLog other = (BudgetExtractCommissionApplicationLog) that;
-        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getApplicationId() == null ? other.getApplicationId() == null : this.getApplicationId().equals(other.getApplicationId()))
-            && (this.getNode() == null ? other.getNode() == null : this.getNode().equals(other.getNode()))
-            && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
-            && (this.getRemarks() == null ? other.getRemarks() == null : this.getRemarks().equals(other.getRemarks()))
-            && (this.getFinancialDirectorNodeOperateTime() == null ? other.getFinancialDirectorNodeOperateTime() == null : this.getFinancialDirectorNodeOperateTime().equals(other.getFinancialDirectorNodeOperateTime()))
-            && (this.getCreateBy() == null ? other.getCreateBy() == null : this.getCreateBy().equals(other.getCreateBy()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getApplicationId() == null) ? 0 : getApplicationId().hashCode());
-        result = prime * result + ((getNode() == null) ? 0 : getNode().hashCode());
-        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
-        result = prime * result + ((getRemarks() == null) ? 0 : getRemarks().hashCode());
-        result = prime * result + ((getFinancialDirectorNodeOperateTime() == null) ? 0 : getFinancialDirectorNodeOperateTime().hashCode());
-        result = prime * result + ((getCreateBy() == null) ? 0 : getCreateBy().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", applicationId=").append(applicationId);
-        sb.append(", node=").append(node);
-        sb.append(", status=").append(status);
-        sb.append(", remarks=").append(remarks);
-        sb.append(", financialDirectorNodeOperateTime=").append(financialDirectorNodeOperateTime);
-        sb.append(", createBy=").append(createBy);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
