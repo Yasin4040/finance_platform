15e439781d016cdb782e37c905416a327f4e9668
lzy20247 <673170360@qq.com>
2022-09-15 11:34:56 +0800

    fix：添加 核算入账任务接口

--- src/main/java/com/jtyjy/finance/manager/service/ExtractAccountEntryTaskService.java
+++ src/main/java/com/jtyjy/finance/manager/service/ExtractAccountEntryTaskService.java
@@ -4,6 +4,8 @@ import com.jtyjy.finance.manager.bean.ExtractAccountEntryTask;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.jtyjy.finance.manager.dto.commission.EntryCompletedDTO;
 
+import java.util.List;
+
 /**
 * @author User
 * @description 针对表【budget_extract_account_entry_task(核算入账)】的数据库操作Service
@@ -13,5 +15,5 @@ public interface ExtractAccountEntryTaskService extends IService<ExtractAccountE
 
     void entryCompleted(EntryCompletedDTO dto);
 
-    void addEntryTask(String sumId);
+    void addEntryTask(Boolean isDelay, List<String> delayList, String extractMonth);
 }
--- src/main/java/com/jtyjy/finance/manager/service/impl/ExtractAccountEntryTaskServiceImpl.java
+++ src/main/java/com/jtyjy/finance/manager/service/impl/ExtractAccountEntryTaskServiceImpl.java
@@ -1,17 +1,26 @@
 package com.jtyjy.finance.manager.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jtyjy.finance.manager.bean.BudgetExtractDelayApplication;
+import com.jtyjy.finance.manager.bean.BudgetExtractPerPayDetail;
 import com.jtyjy.finance.manager.bean.BudgetExtractsum;
 import com.jtyjy.finance.manager.bean.ExtractAccountEntryTask;
 import com.jtyjy.finance.manager.dto.commission.EntryCompletedDTO;
 import com.jtyjy.finance.manager.interceptor.UserThreadLocal;
+import com.jtyjy.finance.manager.mapper.BudgetExtractDelayApplicationMapper;
 import com.jtyjy.finance.manager.mapper.BudgetYearPeriodMapper;
+import com.jtyjy.finance.manager.service.BudgetExtractPerPayDetailService;
 import com.jtyjy.finance.manager.service.BudgetExtractsumService;
 import com.jtyjy.finance.manager.service.ExtractAccountEntryTaskService;
 import com.jtyjy.finance.manager.mapper.ExtractAccountEntryTaskMapper;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
 * @author User
@@ -24,9 +33,13 @@ public class ExtractAccountEntryTaskServiceImpl extends ServiceImpl<ExtractAccou
 
     private final BudgetExtractsumService extractSumService;
     private final BudgetYearPeriodMapper yearMapper;
-    public ExtractAccountEntryTaskServiceImpl(BudgetExtractsumService extractSumService, BudgetYearPeriodMapper yearMapper) {
+    private final BudgetExtractPerPayDetailService perPayDetailService;
+    private final BudgetExtractDelayApplicationMapper delayApplicationMapper;;
+    public ExtractAccountEntryTaskServiceImpl(BudgetExtractsumService extractSumService, BudgetYearPeriodMapper yearMapper, BudgetExtractPerPayDetailService perPayDetailService, BudgetExtractDelayApplicationMapper delayApplicationMapper) {
         this.extractSumService = extractSumService;
         this.yearMapper = yearMapper;
+        this.perPayDetailService = perPayDetailService;
+        this.delayApplicationMapper = delayApplicationMapper;
     }
 
     @Override
@@ -43,15 +56,30 @@ public class ExtractAccountEntryTaskServiceImpl extends ServiceImpl<ExtractAccou
     }
 
     @Override
-    public void addEntryTask(String sumId) {
+    public void addEntryTask(Boolean isDelay, List<String> delayList, String extractMonth) {
+        //延期。
+        List<ExtractAccountEntryTask> taskList = new ArrayList<>();
+        if (isDelay) {
+            List<BudgetExtractDelayApplication> delayApplications = delayApplicationMapper.selectList(new LambdaQueryWrapper<BudgetExtractDelayApplication>()
+                    .in(BudgetExtractDelayApplication::getDelayCode, delayList));
+            for (BudgetExtractDelayApplication delayApplication : delayApplications) {
+                BudgetExtractsum extractSum = extractSumService.getById(delayApplication.getRelationExtractCode());
+                //获取单个任务
+                getSingleEntryTask(extractMonth, taskList, delayApplication.getDelayCode(), extractSum);
+            }
+        }else {
+            List<BudgetExtractsum> curBatchExtractSum = extractSumService.getCurBatchExtractSum(extractMonth);
+
+            for (BudgetExtractsum extractSum : curBatchExtractSum) {
+                getSingleEntryTask(extractMonth, taskList,extractSum.getCode(), extractSum);
+            }
+        }
+        this.saveOrUpdateBatch(taskList);
+
+    }
+
+    private void getSingleEntryTask(String extractMonth, List<ExtractAccountEntryTask> taskList, String nowCode, BudgetExtractsum extractSum) {
         ExtractAccountEntryTask entryTask = new ExtractAccountEntryTask();
-
-
-        BudgetExtractsum extractSum = extractSumService.getById(sumId);
-
-
-        String extractMonth = extractSum.getExtractmonth();
-
         entryTask.setExtractCode(extractSum.getCode());
         entryTask.setExtractMonth(extractSum.getExtractmonth());
 
@@ -64,13 +92,21 @@ public class ExtractAccountEntryTaskServiceImpl extends ServiceImpl<ExtractAccou
         entryTask.setMonthId(monthId);
         entryTask.setMonthName(monthId+"月");
         //用 cache mapper层缓存
-
         entryTask.setStatus(0);
         entryTask.setDeptName(extractSum.getDeptname());
         entryTask.setDeptId(extractSum.getDeptid());
         entryTask.setSumId(String.valueOf(extractSum.getId()));
         entryTask.setExtractCode(extractSum.getCode());
         entryTask.setCreateTime(new Date());
+        //获取发放金额
+        entryTask.setIssuedAmount(getIssuedAmount(nowCode));
+        taskList.add(entryTask);
+    }
+
+    private BigDecimal getIssuedAmount(String extractCode){
+        List<BudgetExtractPerPayDetail> list = perPayDetailService.lambdaQuery().eq(BudgetExtractPerPayDetail::getExtractCode, extractCode).list();
+        return list.stream().map(x -> x.getPayMoney()).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+
     }
 }
 
